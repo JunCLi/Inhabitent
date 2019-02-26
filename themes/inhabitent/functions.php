9b0b4044bf8d7cd1ee806f1d57ca1230619fdb25
kevin JC Li <kevinjcli1994@gmail.com>
2019-02-26 13:45:00 -0500

    added content to find-us page, rough styling of find-us page

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -110,8 +110,8 @@ function demo_theme_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'demo-theme' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
 	));
 
 	register_sidebar(array(
@@ -120,8 +120,8 @@ function demo_theme_widgets_init() {
 		'description'   => esc_html__( 'Add widgets to footer here', 'demo-theme' ),
 		'before_widget' => '<section id="%1$s" class="footer-widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="footer-widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h3 class="footer-widget-title">',
+		'after_title'   => '</h3>',
 	));
 }
 add_action( 'widgets_init', 'demo_theme_widgets_init' );
